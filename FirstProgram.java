b09c3d22a79de6a34526b58390d5a4343d4b2418
Adam Menicucci (Class of 2026) <261280am@internal.salesianum.org>
2023-08-31 10:25:54 -0400

    Notes

---
+++ FirstProgram.java
@@ -0,0 +1,26 @@
+public class FirstProgram {
+        //almost all code in java is written in a class
+        //the name of the class is the name of the file, error pops up if they don't match
+        //almost always start name of class with uppercase
+        //curly brace represents block of code, you have to have two
+    public static void main(String[] args){
+        //main is a function. java must have at least one main function
+        //public makes it accessible outside the file
+        //we want one copy of a static function
+        //void means do not return a value, it just executes code
+        //int, double, boolean, Array are all things you could "return"
+        //functions have parentheses 99% of the time that you put parameters in
+        //functions let you write an equation out once and use it multiple times with multiple values
+        //String[] is an array with a parameter called args (arguments)
+        //Hard brackets [] signal an Array
+        //Functions have their own curly braces
+        //System.out.print vs System.out.println: ln means line, so they won't print "hellohello", and instead "hello (new line) hello"
+
+        System.out.println(x:"hello");
+        System.out.print(s:"hello");
+        System.out.print(s:"hello");
+    }
+    public int test(){
+        return 5;
+    }
+}
